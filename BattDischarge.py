593237ec796136fa3e2d7f353bd1e96b7d373811
Mark Bianchi <markjbianchi@gmail.com>
2020-05-16 21:52:45 -0600

    WIP - have labjack wrapper class basically written

---
+++ BattDischarge.py
@@ -0,0 +1,53 @@
+import u3
+
+
+class BattDischarge:
+    def __init__(self, channels, ser_num=None):
+        self.channel = []
+        for idx, dic in enumerate(channels):
+            self.channel[idx] = {
+                'adc': dic['adc_in'], 'load_en': dic['load_enable'], 'load': dic['load_ohms']
+            }
+        self.d = u3.U3(debug=True)
+        self.d.open(serial=ser_num)
+        # make sure there is an extender attached; need to use special mode ADC if LV model
+        self.d.configU3(FIOAnalog=0xFF, EIODirection=0xFF, EIOState=0x00)
+
+    def _ctrl_load(self, chan, level):
+        if chan < len(self.channel):
+            self.d.setDOState(self.channel[chan].get('load_en'), level)
+        else:
+            print("unable to affect load for channel {}".format(chan))
+
+    def enable_load(self, chan):
+        self._ctrl_load(chan, 0)
+
+    def disable_load(self, chan):
+        self._ctrl_load(chan, 1)
+
+    def disable_all_loads(self):
+        for dic in self.channel:
+            self.d.setDOState(dic['load_en'], 0)
+
+    def measure_channel(self, chan, avgs=0, msec_delay=50):
+        if chan < len(self.channel):
+            v = self.d.getAIN(self.channel[chan].get('adc'))
+            i = v / self.channel[chan].get('load')
+            return v, i
+        else:
+            return 0, 0
+
+
+if __name__ == '___main__':
+    c = [
+        {'adc_in': 0, 'load_enable': 8, 'load_ohms': 751},
+        {'adc_in': 1, 'load_enable': 9, 'load_ohms': 744},
+        {'adc_in': 2, "load_enable": 10, 'load_ohms': 751}
+    ]
+    bd = BattDischarge(c)
+    print(bd)
+    bd.disable_all_loads()
+    for idx in range(len(c)):
+        volts, current = bd.measure_channel(idx)
+        print('chan {}: v={} i={}'.format(idx, volts, current))
+
--- discharger.py
+++ discharger.py
@@ -1,24 +1,32 @@
+#!/usr/bin/env python3
 import signal
 import time
 import sys
 import argparse
 import csv
-import labjackU3
+import BattDischarge
 import json
 
 
 def exit_gracefully(signum, frame):
     # restore the original signal handler as otherwise evil things will happen
-    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
+    # in input when CTRL+C is pressed, and the signal handler is not re-entrant
     signal.signal(signal.SIGINT, original_sigint)
     try:
         if input("\nReally quit? (y/n)> ").lower().startswith('y'):
+            cleanup()
             sys.exit(1)
     except KeyboardInterrupt:
         print("Ok ok, quitting")
+        cleanup()
         sys.exit(1)
     # restore the exit gracefully handler here
     signal.signal(signal.SIGINT, exit_gracefully)
+
+
+def cleanup():
+    # Turn off all loads and anything else to LJ
+    pass
 
 
 def configure_lj(cfg):
@@ -26,20 +34,24 @@ def configure_lj(cfg):
 
 
 def run_program(cfg):
-    '''
-    Print setup, config, etc. Prompt to start or quit.
-    '''
+    # Print setup, config, etc. Prompt to start or quit.
     while True:
         time.sleep(1)
         print("a")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Controls LabJack U3-HV to')
     # store the original SIGINT handler
     original_sigint = signal.getsignal(signal.SIGINT)
     signal.signal(signal.SIGINT, exit_gracefully)
-    with open('default_cfg.json') as cfg_file:
+
+    parser = argparse.ArgumentParser(description='Controls LabJack U3-HV to')
+    parser.add_argument('cfg_file', help='configuration file to set up data acquisition')
+    parser.add_argument('dischrg_init', nargs='?', default='None', help='file containing previous discharge amounts')
+    parser.add_argument('--dry_run', action='store_true', help='option flag to just show output without discharging')
+    args = parser.parse_args()
+
+    with open(args.cfg_file) as cfg_file:
         cfg = json.load(cfg_file)
     configure_lj(cfg)
     run_program(cfg)
